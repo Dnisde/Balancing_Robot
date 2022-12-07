9e8ea40f02db150bab2c0d029c77b9e57ed3bd05
Xiaopeng Huang <80809231+xph3120@users.noreply.github.com>
2022-12-06 19:01:04 -0500

    Update face_recognition_test.py

--- face_recognition_test.py
+++ face_recognition_test.py
@@ -1,6 +1,5 @@
 # import the necessary packages
-from picamera2.array import PiRGBArray
-from picamera2 import PiCamera2
+from picamera2 import Picamera2
 import time
 import cv2
 
@@ -9,7 +8,7 @@ cv2.startWindowThread()
 face_cascade= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 width,height=320,240
-camera = PiCamera2()
+camera = Picamera2()
 camera.resolution = (width,height)
 camera.framerate = 30
 camera.configure(camera.create_preview_configuration(main={"size": (width, height)}))
@@ -18,7 +17,7 @@ time.sleep(1)
 
 while True:
         frame = camera.capture_array():
-        frame=cv2.flip(frame,-1)
+        frame=cv2.flip(frame, 1)
         gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)    
 
         #detect face coordinates x,y,w,h
@@ -35,8 +34,8 @@ while True:
             error_x=160-face_centre_x
             error_y=120-face_centre_y
 
-            print('pixelerrorx=',error_x,'valx=',valx)
-            print('pixelerrory=',error_y,'valy=',valy)
+            print('pixelerrorx=',error_x)
+            print('pixelerrory=',error_y)
                 
             if(c==1):
                 frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),6)
@@ -44,7 +43,6 @@ while True:
         cv2.imshow('frame',frame) #display image
         
         key = cv2.waitKey(1) & 0xFF
-        rawCapture.truncate(0)
         if key == ord("q"):
             break
 
